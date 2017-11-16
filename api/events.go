737ebeae14e4bbe2898828fc2abb97f7b1e2c3ff
Ryan Norris <rnorris@palantir.com>
2017-11-17 03:15:18 +0900

    Create tick event type

--- api/events.go
+++ api/events.go
@@ -7,28 +7,28 @@ const (
 	EventWorldUpdate events.Type = iota
 	EventMoveIntent
 	EventQuit
+	EventTick
 )
+
+const (
+	Quit = basicEvent(EventQuit)
+	Tick = basicEvent(EventTick)
+)
+
+type basicEvent events.Type
+
+func (b basicEvent) Type() events.Type { return events.Type(b) }
 
 type WorldUpdate struct {
 	Player   PlayerData
 	Entities []EntityData
 }
 
-func (u WorldUpdate) Type() events.Type {
-	return EventWorldUpdate
-}
+func (u WorldUpdate) Type() events.Type { return EventWorldUpdate }
 
 type MoveIntent struct {
 	X int
 	Y int
 }
 
-func (i MoveIntent) Type() events.Type {
-	return EventMoveIntent
-}
-
-type Quit struct{}
-
-func (i Quit) Type() events.Type {
-	return EventQuit
-}
+func (i MoveIntent) Type() events.Type { return EventMoveIntent }
